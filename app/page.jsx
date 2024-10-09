81e0aeecb0212815c1beb2001e357f0963f2e54d
MINHAJ MM <minhaj99466@gmail.com>
2024-10-09 22:54:15 +0530

    resume updated

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
             <h1 className="h2 mb-6">Hi I'm <br /><span className="text-accent">Minhaj.mm</span></h1>
             <p className="max-w-[500px] mb-9">I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
             <div className="flex flex-col xl:flex-row gap-8 items-center">
-             <Link href='/assets/resume/Minhaj_Resume_FullStack_1.5yrs.pdf'><Button  variant="outline" size="lg" className="hover:text-primary uppercase flex items-center gap-2 hover-transition-all duration-700 rounded-full">Download Resume  <FaFileDownload className="text-xl "/></Button></Link> 
+             <Link href='/assets/resume/Minhaj_Resume_1.5year_FullStack.pdf'><Button  variant="outline" size="lg" className="hover:text-primary uppercase flex items-center gap-2 hover-transition-all duration-700 rounded-full">Download Resume  <FaFileDownload className="text-xl "/></Button></Link> 
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="flex w-9 h-9 text-accent hover:bg-accent border border-accent rounded-full justify-center items-center hover:text-primary hover-transition-all duration-1000" />
               </div>
